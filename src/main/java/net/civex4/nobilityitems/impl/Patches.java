e64ae17a17650257bbe177e4ff79d57e89314853
Joe <burtonjae@hotmail.co.uk>
2021-02-20 23:52:30 +0000

    Waterlogged option is now functional

--- src/main/java/net/civex4/nobilityitems/impl/Patches.java
+++ src/main/java/net/civex4/nobilityitems/impl/Patches.java
@@ -9,12 +9,16 @@ import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.wrappers.BlockPosition;
 import com.comphenix.protocol.wrappers.BukkitConverters;
 import com.comphenix.protocol.wrappers.WrappedBlockData;
+import net.bytebuddy.jar.asm.Label;
 import net.bytebuddy.jar.asm.MethodVisitor;
 import net.bytebuddy.jar.asm.Opcodes;
 import net.bytebuddy.jar.asm.Type;
+import net.civex4.nobilityitems.NobilityBlock;
+import net.civex4.nobilityitems.NobilityItems;
 import net.civex4.nobilitypatch.CallbackKey;
 import net.civex4.nobilitypatch.NobilityPatch;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.data.BlockData;
 
@@ -24,6 +28,7 @@ import java.lang.reflect.Modifier;
 public class Patches {
     private static final Class<?> blockData = MinecraftReflection.getMinecraftClass("BlockBase$BlockData");
     private static final Class<?> iBlockData = MinecraftReflection.getIBlockDataClass();
+    private static final Class<?> blockPosition = MinecraftReflection.getBlockPositionClass();
     private static final MethodAccessor blockData_asIBlockData = Accessors.getMethodAccessor(FuzzyReflection.fromClass(blockData, true).getMethod(FuzzyMethodContract.newBuilder()
             .requireModifier(Modifier.ABSTRACT)
             .returnTypeExact(iBlockData)
@@ -38,8 +43,12 @@ public class Patches {
             .build()));
     private static final MethodAccessor flagDirty = Accessors.getMethodAccessor(FuzzyReflection.fromClass(chunkProviderServer).getMethod(FuzzyMethodContract.newBuilder()
             .nameExact("flagDirty")
-            .parameterExactArray(MinecraftReflection.getBlockPositionClass())
+            .parameterExactArray(blockPosition)
             .build()));
+
+    private static final Class<?> fluid = MinecraftReflection.getMinecraftClass("Fluid");
+    private static final Method getFluidMethod = FuzzyReflection.fromClass(blockData).getMethodByParameters("getFluid", fluid, new Class[0]);
+    private static final MethodAccessor getFluid = Accessors.getMethodAccessor(getFluidMethod);
 
 
     private static final EquivalentConverter<World> WORLD_CONVERTER = BukkitConverters.getWorldConverter();
@@ -48,6 +57,7 @@ public class Patches {
 
     public static void apply() {
         addNeighborPlacementCallback();
+        addWaterloggedCallback();
     }
 
     private static void addNeighborPlacementCallback() {
@@ -68,11 +78,9 @@ public class Patches {
             }
         });
 
-        Class<?> iBlockData = MinecraftReflection.getIBlockDataClass();
         Class<?> enumDirection = MinecraftReflection.getMinecraftClass("EnumDirection");
         Class<?> generatorAccess = MinecraftReflection.getMinecraftClass("GeneratorAccess");
-        Class<?> blockPos = MinecraftReflection.getBlockPositionClass();
-        Method method = FuzzyReflection.fromClass(blockData).getMethodByParameters("updateState", iBlockData, new Class[] {enumDirection, iBlockData, generatorAccess, blockPos, blockPos});
+        Method method = FuzzyReflection.fromClass(blockData).getMethodByParameters("updateState", iBlockData, new Class[] {enumDirection, iBlockData, generatorAccess, blockPosition, blockPosition});
         NobilityPatch.transformMethod(method, visitor -> new MethodVisitor(Opcodes.ASM9, visitor) {
             @Override
             public void visitInsn(int opcode) {
@@ -88,9 +96,49 @@ public class Patches {
         });
     }
 
+    private static final Object EMPTY_FLUID = getFluid.invoke(BLOCK_DATA_CONVERTER.getGeneric(WrappedBlockData.createData(Material.AIR)));
+    private static final Object WATER_FLUID = getFluid.invoke(BLOCK_DATA_CONVERTER.getGeneric(WrappedBlockData.createData(Material.WATER)));
+
+    private static void addWaterloggedCallback() {
+        CallbackKey<WaterloggedDelegate> waterloggedCallback = NobilityPatch.registerCallback(WaterloggedDelegate.class, _this -> {
+            BlockData blockData = (BlockData) craftBlockData_fromData.invoke(null, blockData_asIBlockData.invoke(_this));
+            NobilityBlock nobilityBlock = NobilityItems.getBlock(blockData);
+            if (nobilityBlock == null) {
+                return null;
+            }
+            return nobilityBlock.isWaterlogged() ? WATER_FLUID : EMPTY_FLUID;
+        });
+
+        NobilityPatch.transformMethod(getFluidMethod, visitor -> new MethodVisitor(Opcodes.ASM9, visitor) {
+            private boolean visitedLabel = false;
+            @Override
+            public void visitLabel(Label label) {
+                mv.visitLabel(label);
+                if (!visitedLabel) {
+                    visitedLabel = true;
+                    mv.visitVarInsn(Opcodes.ALOAD, 0); // this
+                    NobilityPatch.invokeCallback(mv, waterloggedCallback);
+                    mv.visitTypeInsn(Opcodes.CHECKCAST, Type.getInternalName(fluid));
+                    mv.visitInsn(Opcodes.DUP);
+                    Label endIf = new Label();
+                    mv.visitJumpInsn(Opcodes.IFNULL, endIf);
+                    mv.visitInsn(Opcodes.ARETURN);
+                    mv.visitLabel(endIf);
+                    mv.visitInsn(Opcodes.POP);
+                }
+            }
+        });
+    }
+
     @FunctionalInterface
     public interface OverrideNeighborPlacementDelegate {
         @SuppressWarnings("unused")
         Object /*IBlockData*/ overrideNeighborPlacementState(Object /*IBlockData*/ state, Object /*BlockBase$BlockData*/ _this, Object /*GeneratorAccess*/ world, Object /*BlockPosition*/ pos);
+    }
+
+    @FunctionalInterface
+    public interface WaterloggedDelegate {
+        @SuppressWarnings("unused")
+        Object /*Fluid*/ getFluid(Object /*BlockBase$BlockData*/ _this);
     }
 }
